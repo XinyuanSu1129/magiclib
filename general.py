51daf7135420f1218ef026357cbd008fc8353101
sumiaomiao <xinyuan_su@126.com>
2026-06-17 13:35:45 +0800

    Optimized the code, see the report for details.

--- general.py
+++ general.py
@@ -941,12 +941,14 @@ class Function:
         new_data = []
 
         sr, sg, sb = seek_color[:3]
+        tr, tg, tb, ta = target_color
 
         for r, g, b, a in data:
             if (abs(r - sr) <= tolerance and
                     abs(g - sg) <= tolerance and
                     abs(b - sb) <= tolerance):
-                new_data.append(target_color)
+                # 只替换 RGB，保留原 Alpha
+                new_data.append((tr, tg, tb, a))
             else:
                 new_data.append((r, g, b, a))
 
